bc64ba0ea594d5aee7c609f401283d500d129e4a
Bilgin <kocakbilgin@gmail.com>
2022-10-04 12:47:58 +0300

    ask listeners added

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,25 +1,68 @@
 // Copyright 2020-2022 OnFinality Limited authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 import {
-  Ask
+  Ask,
+  BlockEntity
 } from "../types";
 import {
   AvalancheBlock,
   AvalancheTransaction,
   AvalancheLog,
 } from "@subql/types-avalanche";
+import assert from "assert";
 
 
-export async function handleCreatedAsk(event: AvalancheLog): Promise<void> {
-  console.log("*****************************************************************************************");
-  console.log("event.args", event.args);
-  // const ask = new Ask(event.args.tokenId.toString());
-  // ask.tokenID = event.args.tokenId;
-  // ask.tokenContract = event.args.tokenContract;
-  // ask.seller = event.args.ask.seller;
-  // ask.sellerFundsRecipient = event.args.ask.sellerFundsRecipient;
-  // ask.askCurrency = event.args.ask.askCurrency;
-  // ask.askPrice = event.args.ask.askPrice;
-  // ask.createdAtTimestamp = event.block.timestamp;
-  // await ask.save();
+export async function handleAskCreated(event: AvalancheLog): Promise<void> {
+  logger.info(`Ask created: ${event.args}`);
+  assert(event.args, 'No event args');
+  
+  if (event.args.tokenId){
+    const ask = new Ask(event.args.tokenId.toString() + "-" + event.args.tokenContract.toString());
+    ask.tokenID = event.args.tokenId;
+    ask.tokenContract = event.args.tokenContract;
+    ask.seller = event.args.ask.seller;
+    ask.sellerFundsRecipient = event.args.ask.sellerFundsRecipient;
+    ask.askCurrency = event.args.ask.askCurrency;
+    ask.askPrice = event.args.ask.askPrice;
+    ask.createdAtTimestamp = event.block.timestamp;
+    logger.info("Ask AAA price updated for tokenID: " + event.args.tokenId.toString() + " and tokenContract: " + event.args.tokenContract.toString());
+    await ask.save();
+  }
 }
+
+export async function handleAskPriceUpdated(event: AvalancheLog): Promise<void> {
+  assert(event.args, 'No event args');
+
+  if (event.args.tokenId){
+    const ask = await Ask.get(event.args.tokenId.toString() + "-" + event.args.tokenContract.toString());
+    ask.askCurrency = event.args.ask.askCurrency;
+    ask.askPrice = event.args.ask.askPrice;
+    await ask.save();
+  }
+}
+
+export async function handleAskCanceled(event: AvalancheLog): Promise<void> {
+  assert(event.args, 'No event args');
+
+  if (event.args.tokenId){
+    const id = event.args.tokenId.toString() + "-" + event.args.tokenContract.toString();
+    logger.info("Deleting ask with id: " + id);
+    await Ask.remove(id);
+  }
+}
+
+export async function handleAskFilled(event: AvalancheLog): Promise<void> {
+  assert(event.args, 'No event args');
+
+  if (event.args.tokenId){
+    const id = event.args.tokenId.toString() + "-" + event.args.tokenContract.toString();
+    await store.remove('Ask', id);
+  }
+}
+
+
+// export async function handleBlock(block: AvalancheBlock): Promise<void> {
+//   const record = new BlockEntity(block.hash);
+//   record.height = BigInt(block.number);
+//   await record.save();
+// }
