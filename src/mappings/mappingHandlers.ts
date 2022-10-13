6c84547be8f47445982663fa43984b484bf0c8ec
Bilgin <kocakbilgin@gmail.com>
2022-10-13 17:13:44 +0300

    auction and offer added

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -2,7 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 import {
   Ask,
-  BlockEntity
+  ReserveAuction,
+  Offer
 } from "../types";
 import {
   AvalancheBlock,
@@ -11,7 +12,7 @@ import {
 } from "@subql/types-avalanche";
 import assert from "assert";
 
-
+// ASK HANDLERS
 export async function handleAskCreated(event: AvalancheLog): Promise<void> {
   assert(event.args, 'No event args');
 
@@ -60,9 +61,119 @@ export async function handleAskFilled(event: AvalancheLog): Promise<void> {
   }
 }
 
+// AUCTION HANDLERS
 
-// export async function handleBlock(block: AvalancheBlock): Promise<void> {
-//   const record = new BlockEntity(block.hash);
-//   record.height = BigInt(block.number);
-//   await record.save();
-// }
+export async function handleAuctionCreated(event: AvalancheLog): Promise<void> {
+  assert(event.args, 'No event args');
+
+  if (event.args.tokenId){
+    const reserveAuction = new ReserveAuction(event.args.tokenId.toString() + "-" + event.args.tokenContract.toString());
+    reserveAuction.tokenID = event.args.tokenId;
+    reserveAuction.tokenContract = event.args.tokenContract;
+    reserveAuction.seller = event.args.auction.seller;
+    reserveAuction.sellerFundsRecipient = event.args.auction.sellerFundsRecipient;
+    reserveAuction.currency = event.args.auction.currency;
+    reserveAuction.reservePrice = event.args.auction.reservePrice;
+    reserveAuction.duration = event.args.auction.duration;
+    reserveAuction.startTime = event.args.auction.startTime;
+    reserveAuction.createdAtTimestamp = event.block.timestamp;
+    // logger.info("Ask created: " + event.args.tokenId.toString() + " and tokenContract: " + event.args.tokenContract.toString());
+    await reserveAuction.save();
+  }
+}
+
+export async function handleAuctionReservePriceUpdated(event: AvalancheLog): Promise<void> {
+  assert(event.args, 'No event args');
+
+  if (event.args.tokenId){
+    const reserveAuction = await ReserveAuction.get(event.args.tokenId.toString() + "-" + event.args.tokenContract.toString());
+    reserveAuction.reservePrice = event.args.auction.reservePrice;
+    // logger.info("Ask created: " + event.args.tokenId.toString() + " and tokenContract: " + event.args.tokenContract.toString());
+    await reserveAuction.save();
+  }
+}
+
+export async function handleAuctionCanceled(event: AvalancheLog): Promise<void> {
+  assert(event.args, 'No event args');
+
+  if (event.args.tokenId){
+    const id = event.args.tokenId.toString() + "-" + event.args.tokenContract.toString();
+    // logger.info("Ask created: " + event.args.tokenId.toString() + " and tokenContract: " + event.args.tokenContract.toString());
+    await ReserveAuction.remove(id);
+  }
+}
+
+export async function handleAuctionBid(event: AvalancheLog): Promise<void> {
+  assert(event.args, 'No event args');
+
+  if (event.args.tokenId){
+    const reserveAuction = new ReserveAuction(event.args.tokenId.toString() + "-" + event.args.tokenContract.toString());
+    reserveAuction.duration = event.args.auction.duration;
+    reserveAuction.highestBid = event.args.auction.highestBid;
+    reserveAuction.highestBidder = event.args.auction.highestBidder;
+    reserveAuction.firstBidTime = event.args.auction.firstBidTime;
+    reserveAuction.updatedAtTimestamp = event.block.timestamp;
+    // logger.info("Ask created: " + event.args.tokenId.toString() + " and tokenContract: " + event.args.tokenContract.toString());
+    await reserveAuction.save();
+  }
+}
+
+export async function handleAuctionEnded(event: AvalancheLog): Promise<void> {
+  assert(event.args, 'No event args');
+
+  if (event.args.tokenId){
+    const id = event.args.tokenId.toString() + "-" + event.args.tokenContract.toString();
+    // logger.info("Ask created: " + event.args.tokenId.toString() + " and tokenContract: " + event.args.tokenContract.toString());
+    await ReserveAuction.remove(id);
+
+  }
+}
+
+// OFFER HANDLERS
+
+export async function handleOfferCreated(event: AvalancheLog): Promise<void> {
+  assert(event.args, 'No event args');
+
+  if (event.args.tokenId){
+    const offer = new Offer(event.args.id.toString());
+    offer.tokenID = event.args.tokenId;
+    offer.tokenContract = event.args.tokenContract;
+    offer.currency = event.args.offer.currency;
+    offer.offerPrice = event.args.offer.amount;
+    offer.maker = event.args.offer.maker;
+    offer.findersFeeBps = event.args.offer.findersFeeBps;
+    offer.createdAtTimestamp = event.block.timestamp;
+    // logger.info("Ask created: " + event.args.tokenId.toString() + " and tokenContract: " + event.args.tokenContract.toString());
+    await offer.save();
+  }
+}
+
+export async function handleOfferUpdated(event: AvalancheLog): Promise<void> {
+  assert(event.args, 'No event args');
+
+  if (event.args.tokenId){
+    const offer = await Offer.get(event.args.id.toString());
+    offer.currency = event.args.offer.currency;
+    offer.offerPrice = event.args.offer.amount;
+    await offer.save();
+  }
+}
+
+export async function handleOfferCanceled(event: AvalancheLog): Promise<void> {
+  assert(event.args, 'No event args');
+
+  if (event.args.tokenId){
+    const id = event.args.id.toString();
+    await Offer.remove(id);
+  }
+}
+
+export async function handleOfferFilled(event: AvalancheLog): Promise<void> {
+  assert(event.args, 'No event args');
+
+  if (event.args.tokenId){
+    const id = event.args.id.toString();
+    await Offer.remove(id);
+  }
+}
+
